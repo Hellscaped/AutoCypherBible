bf65d166feb024c82bca7fd6d589edb468ff3f00
Hellscaped <tappsyo@gmail.com>
2024-01-29 18:02:45 +0000

    Made the JSON tool classful (can be used as an API now for programatic use)

--- jsoncrypter.py
+++ jsoncrypter.py
@@ -1,32 +1,34 @@
 import sys, json, numpy as np, hashlib, time
+
+
+class CypherResolver:
+    def __init__(self, mappingsjson):
+        self.mappings = json.load(open(mappingsjson,"r"))
+        self.reverse = {}
+        for k,v in self.mappings.items():
+            for i in v:
+                self.reverse[i] = k
+    def encode(self, s):
+        t = ""
+        for i in s:
+            if i.upper() in self.mappings:
+                t += str(np.random.choice(self.mappings[i.upper()]))
+                if i != s[-1]:
+                    t += " "
+        return t
+    def decode(self, s):
+        t = ""
+        s = s.split(" ")
+        g = []
+        for i in s:
+            if int(i) in self.reverse:
+                t += self.reverse[int(i)]
+        return t
+
 def seed():
     return int(hashlib.sha256(str(time.time()).encode()).hexdigest(), 16) % 10**8
 np.random.seed(seed())
 
-to_encoding = json.load(open(sys.argv[1],"r"))
-from_encoding = {}
-for k,v in to_encoding.items():
-    for i in v:
-        from_encoding[i] = k
-
-def encode(s):
-    t = ""
-    for i in s:
-        if i.upper() in to_encoding:
-            t += str(np.random.choice(to_encoding[i.upper()]))
-            if i != s[-1]:
-                t += " "
-    return t
-
-def decode(s):
-    t = ""
-    s = s.split(" ")
-    g = []
-    for i in s:
-        if int(i) in from_encoding:
-            t += from_encoding[int(i)]
-    return t
-    
 def after_argv(ind):
     s = ""
     for i in range(ind,len(sys.argv)):
@@ -36,7 +38,8 @@ def after_argv(ind):
     return s
 
 if __name__ == "__main__":
+    c = CypherResolver(sys.argv[1])
     if sys.argv[2] == "encode":
-        print(encode(after_argv(3)))
+        print(c.encode(after_argv(3)))
     elif sys.argv[2] == "decode":
-        print(decode(after_argv(3)))
+        print(c.decode(after_argv(3)))
